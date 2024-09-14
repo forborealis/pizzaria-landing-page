8d1cb08b3942bb46c931312606d9e7acca7cae5a
forborealis <hannahaurora.busto@tup.edu.ph>
2024-09-14 19:00:53 +0800

    about

---
+++ src/Components/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+    return (
+        <div className="about-section-container">
+            <div className="about-background-image-container">
+                <img src={AboutBackground} alt="about-background"/>
+            </div>
+            <div className="about-section-image-container">
+                <img src={AboutBackgroundImage} alt="about-image"/>
+            </div>
+            <div className="about-section-text-container">
+                <p className="primary-subheading">About</p>
+                <h1 className="primary-heading">Pizza Crafted with Passion</h1>
+                <p className="primary-text">
+                Our journey started with a simple love for authentic flavors, and today,
+                we continue to serve delicious, perfectly baked pizzas that bring people together. Whether you're here for a quick bite or a family gathering, you're always part of our pizza-loving community!
+                </p>
+                <div className="about-buttons-container">
+                    <button className="secondary-button">Learn More</button>
+                    <button className="watch-video-button">
+                        <BsFillPlayCircleFill/>Watch Video
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;

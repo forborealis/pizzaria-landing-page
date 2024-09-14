3dbe3bd1cfd45f18cbba862402585a042ce171dc
forborealis <hannahaurora.busto@tup.edu.ph>
2024-09-14 20:20:31 +0800

    contact

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+    <div className="contact-page-wrapper">
+        <h1 className="primary-heading">Have a Question in Mind?</h1>
+        <div className="contact-form-container">
+            <input type="text" placeholder="youremail@mail.com"/>
+            <button className="secondary-button">Submit</button>
+        </div>
+    </div>
+    );
+};
+
+
+export default Contact;

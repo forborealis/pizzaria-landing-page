bd69e336ae1a0754dcbc9e1cfffa38bae7309870
forborealis <hannahaurora.busto@tup.edu.ph>
2024-09-14 17:02:52 +0800

    navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "./Navbar";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+    <div className="home-container">
+        <Navbar/>
+        </div>
+    );
 };
 
 export default Home;

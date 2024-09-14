4f13db90316c2d8cfda082f1784ad9d7d47c5a48
forborealis <hannahaurora.busto@tup.edu.ph>
2024-09-14 18:27:51 +0800

    home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,33 @@
 import React from "react";
 import Navbar from "./Navbar";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight} from "react-icons/fi";
 
 const Home = () => {
     return (
     <div className="home-container">
         <Navbar/>
+        <div className="home-banner-container">
+            <div className="home-bannerImage-container">
+                <img src={BannerBackground} alt="banner"/>
+            </div>
+            <div className="home-text-section">
+                <h1 className="primary-heading">
+                Where Every Slice is a Masterpiece!
+                </h1>
+                    <p className="primary-text">
+                    Welcome to the home of handcrafted, delicious pizzas
+                    made from the freshest ingredients. Indulge in our mouthwatering variety and experience the perfect slice, every time.
+                    </p>
+                    <button className="secondary-button">
+                        Order Now <FiArrowRight/>
+                    </button>
+            </div>
+            <div className="home-image-container">
+                <img src={BannerImage} alt="banner"/>
+            </div>
+        </div>
         </div>
     );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Logo from "../Assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3} from "react-icons/hi2";
 import {
     Box,
     Drawer,
+    List,
     ListItem,
     ListItemButton,
     ListItemIcon,
@@ -18,7 +18,7 @@ import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 
 const Navbar = () => {
-    const [open, setOpenMenu] = useState(false);
+    const [openMenu, setOpenMenu] = useState(false);
     const menuOptions = [
         {
             text: "Home",
@@ -45,7 +45,6 @@ const Navbar = () => {
     return (
         <nav>
     <div className="nav-logo-container">
-        <img src={Logo} alt="logo" className="nav-logo"/>
         </div>
         <div className="navbar-links-container">
             <a href="">Home</a>
@@ -60,6 +59,26 @@ const Navbar = () => {
         <div className="navbar-menu-container">
             <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
         </div>
+        <Drawer open={openMenu} onClose={() => setOpenMenu(false)}
+            anchor="right">
+            <Box
+            sx={{width: 250}}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+            >
+            <List>
+                {menuOptions.map((item) => (
+                    <ListItem key={item.text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            </Box>
+        </Drawer>
     </nav>
     );
 };
